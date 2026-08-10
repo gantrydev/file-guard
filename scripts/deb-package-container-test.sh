43040e2538fc8fcf0829d65ce3e065a8aab4984c
dianfishekqi <dian@fishekqi.com>
2026-08-10 21:39:04 +0200

    chore(release): prepare v0.1.19

--- scripts/deb-package-container-test.sh
+++ scripts/deb-package-container-test.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 deb=${FILE_GUARD_DEB:-/tmp/file-guard.deb}
-expected_version=${FILE_GUARD_EXPECTED_VERSION:-0.1.18}
+expected_version=${FILE_GUARD_EXPECTED_VERSION:-0.1.19}
 require_fuse=${FILE_GUARD_REQUIRE_FUSE:-1}
 
 fail() {
